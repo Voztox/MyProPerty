8fc7ef6da2e407d5b660046ffe38cceaf00c2041
Mert <117932184+Voztox@users.noreply.github.com>
2024-04-22 18:05:42 +0100

    Property.php only for viewing
    not sure if it should make it to the final project.

---
+++ ass03/property.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Property</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/2.0.5/css/dataTables.bootstrap5.css">
+</head>
+
+<body>
+    <table id="example" class="table table-striped" style="width:100%">
+        <thead>
+            <tr>
+                <th>PropertyId</th>
+                <th>Bed Amount</th>
+                <th>Price</th>
+                <th>Area</th>
+                <th>Length Tenancy</th>
+                <th>Contract Start</th>
+                <th>Contarct End</th>
+                <th>CustomerId</th>
+                <th>Desc</th>
+                <th>Title</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+            require("../../mysql_connect.php");
+            $sql = "SELECT * FROM property";
+            $result = $conn->query($sql);
+
+            if (!$result) {
+                die("Error executing the query: " . mysqli_error($conn));
+            }
+            if ($result && mysqli_num_rows($result) > 0) {
+                // Fetch data from $result and display
+                while ($row = mysqli_fetch_assoc($result)) {
+                    echo "<tr>";
+                    echo "<td>" . $row['propertyID'] . "</td>";
+                    echo "<td>" . $row['bedAmount'] . "</td>";
+                    echo "<td>" . $row['price'] . "</td>";
+                    echo "<td>" . $row['area'] . "</td>";
+                    echo "<td>" . $row['lengthOfTenancy'] . "</td>";
+                    echo "<td>" . $row['contractStart'] . "</td>";
+                    echo "<td>" . $row['contractEnd'] . "</td>";
+                    echo "<td>" . $row['customerID'] . "</td>";
+                    echo "<td>" . $row['desc'] . "</td>";
+                    echo "<td>" . $row['title'] . "</td>";
+                    echo "</tr>";
+                }
+            } else {
+                echo "<tr><td colspan='10'>No records found.</td></tr>";
+            }
+            ?>
+
+        </tbody>
+    </table>
+    <script src="script.js"></script>
+    <script src="https://code.jquery.com/jquery-3.7.1.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.0/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdn.datatables.net/2.0.5/js/dataTables.js"></script>
+    <script src="https://cdn.datatables.net/2.0.5/js/dataTables.bootstrap5.jss"></script>
+</body>
+
+</html>
